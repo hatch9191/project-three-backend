d3a75a01cb2193eb3bfd4a6a05ce49c14d4add68
Harry Evans <henryjohnevans@icloud.com>
2021-09-03 15:01:21 +0100

    delpoying heroku

--- index.js
+++ index.js
@@ -1,16 +1,6 @@
-import express from 'express'
-import logger from './lib/logger.js'
 import { connectDb } from './db/helpers.js'
 import { port } from './config/environment.js'
-import router from './config/router.js'
-import errorHandler from './lib/errorHandler.js'
-
-const app = express()
-
-app.use(express.json())
-app.use('/', logger)
-app.use('/api', router)
-app.use(errorHandler)
+import app from './app.js'
 
 async function startServer() {
   try {
